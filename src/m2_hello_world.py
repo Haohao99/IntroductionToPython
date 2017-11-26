0cb5fdc9996a480ebcfebfcd00f73f491e6ef32b
huh2 <huh2@rose-hulman.edu>
2017-11-26 17:37:27 -0500

    Assignmemt DONE

--- src/m2_hello_world.py
+++ src/m2_hello_world.py
@@ -59,7 +59,7 @@ print('Hello, World')
 #   Look at the Console to be sure that your string printed as expected.
 #
 ########################################################################
-print('hello haoo')
+print('hello hao')
 ########################################################################
 #
 # DONE: 4.
